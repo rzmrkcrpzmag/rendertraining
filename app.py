a8399fd1ffb4ede49e1c52db51ae5f5decaacba3
Angelie Buen <angeliebuen@gmail.com>
2024-08-15 21:25:19 +0800

    app in

--- app.py
+++ app.py
@@ -1,1 +1,19 @@
-# test change
+from flask import Flask, render_template, request
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    result_age = 0
+    if request.method == 'POST':
+        try:
+            age = float(request.form['age'])
+            result_age = age + 6
+        except ValueError:
+            result_age = ""
+            
+    return render_template("index.html", age=result_age)
+
+
+if __name__ == '__main__':
+  app.run(debug=True)
